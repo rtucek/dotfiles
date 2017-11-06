25607d5abff24a10099307b9db72486c99d7520f
Rudolf Tucek <tucek.rudolf@gmail.com>
2017-11-06 01:47:52 +0100

    Add fzf

---
+++ dotfiles/.fzf.bash
@@ -0,0 +1,14 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/rtucek/.fzf/bin* ]]; then
+  export PATH="$PATH:/home/rtucek/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/rtucek/.fzf/shell/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/rtucek/.fzf/shell/key-bindings.bash"
+
