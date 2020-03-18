e06a50055ad0b2b5f28c38e9977c7b6ed0fac5a2
Rudolf Tucek <tucek.rudolf@gmail.com>
2020-03-18 10:26:29 +0100

    Explicitly disable while typing

--- dotfiles/.config/i3/touchpad.sh
+++ dotfiles/.config/i3/touchpad.sh
@@ -3,3 +3,4 @@
 TOUCHPAD="$(xinput --list --name-only | grep 'Touchpad')"
 
 xinput --set-prop "$TOUCHPAD" "libinput Natural Scrolling Enabled" 1
+xinput --set-prop "$TOUCHPAD" "libinput Disable While Typing Enabled" 1
