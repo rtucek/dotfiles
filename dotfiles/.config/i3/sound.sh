be7594f0170b7b4788e4c51fb4167063da938407
Rudolf Tucek <tucek.rudolf@gmail.com>
2021-11-25 02:24:27 +0100

    Unmute if muted and volume change requested

--- dotfiles/.config/i3/sound.sh
+++ dotfiles/.config/i3/sound.sh
@@ -8,6 +8,12 @@ pactl "$1" $SINK "$2" > /dev/null
 VOLUME=$(pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(($SINK + 1)) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
 MUTE=$(pactl list sinks | grep '^[[:space:]]Mute:' | head -n $(($SINK + 1)) | tail -n 1 | sed -e 's,.*\(yes\|no\).*,\1,')
 
+# As as special case, unmute if already muted, but volume change request
+if [[ "$MUTE" == "yes" && "$1" == "set-sink-volume" ]]; then
+	pactl "set-sink-mute" $SINK "toggle"
+	MUTE="no"
+fi
+
 if [[ $VOLUME == 0 || "$MUTE" == "yes" ]]; then
 	# Show the sound muted notification
 	notify-send \
