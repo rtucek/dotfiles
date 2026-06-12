212686832f14a369505f3461f91a2460ed513328
Rudolf Tucek <tucek.rudolf@gmail.com>
2026-06-12 21:31:39 +0200

    waybar: have capslock-check.sh reference proper coreutil bin paths

--- home/scripts/capslock-check.nix
+++ home/scripts/capslock-check.nix
@@ -1,9 +1,15 @@
 { pkgs, lib, ... }:
+let
+  find = "${lib.getExe pkgs.findutils}";
+  cat = "${lib.getBin pkgs.coreutils-full}/bin/cat";
+  head = "${lib.getBin pkgs.coreutils-full}/bin/head";
+  jq = "${lib.getExe pkgs.jq}";
+in
 pkgs.writeShellScriptBin "capslock-check.sh" ''
   while true
   do
     # Check if caps-lock LED is enabled for first-best input device
-    CAPS_ON="$(cat "$(find /sys/class/leds/input*::capslock/ -name 'brightness' | head -1)")"
+    CAPS_ON="$(${cat} "$(${find} /sys/class/leds/input*::capslock/ -name 'brightness' | ${head} -1)")"
 
     # empty string causes waybar to show nothing
     TEXT="{ \"text\": \"\"}"
@@ -12,7 +18,7 @@ pkgs.writeShellScriptBin "capslock-check.sh" ''
     	TEXT="{ \"text\": \"󰘲\", \"class\": \"active\"}"
     fi
 
-    echo "$TEXT" | ${lib.getExe pkgs.jq} --unbuffered --compact-output
+    echo "$TEXT" | ${jq} --unbuffered --compact-output
     sleep 1
   done
 ''
