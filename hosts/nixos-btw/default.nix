5a43ecc0a8489d954390874adbc4fe29fb969732
Rudolf Tucek <tucek.rudolf@gmail.com>
2026-06-19 20:49:35 +0200

    Add dummy config for nixvim

--- hosts/nixos-btw/default.nix
+++ hosts/nixos-btw/default.nix
@@ -23,6 +23,20 @@
     defaultEditor = true;
     # TODO: enable, once using home manager
     # vimdiffAlias = true;
+    enableMan = true;
+    enablePrintInit = true;
+    waylandSupport = true;
+
+    withNodeJs = true;
+    withPerl = true;
+    withPython3 = true;
+    withRuby = true;
+
+    editorconfig = {
+      enable = true;
+    };
+
+    colorschemes.gruvbox.enable = true;
   };
 
   security.pam.services.hyprlock = {
