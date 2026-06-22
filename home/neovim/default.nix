28f935ed1f08ef789b1641536fa19b6cbcc328aa
Rudolf Tucek <tucek.rudolf@gmail.com>
2026-06-22 20:10:30 +0200

    nixvim: migrate global settings

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -1,28 +1,12 @@
 { ... }:
 {
   imports = [
+    ./settings.nix
+
     ./languages
   ];
 
   programs.nixvim = {
     enable = true;
-    defaultEditor = true;
-    vimdiffAlias = true;
-    enableMan = true;
-    waylandSupport = true;
-
-    # Language providers
-    withNodeJs = true;
-    withPerl = true;
-    withPython3 = true;
-    withRuby = true;
-
-    editorconfig = {
-      enable = true;
-    };
-
-    colorschemes.onedark.enable = true;
-
-    clipboard.register = "unnamedplus";
   };
 }
---
+++ home/neovim/settings.nix
@@ -0,0 +1,74 @@
+{ ... }:
+{
+  programs.nixvim = {
+    defaultEditor = true;
+    vimdiffAlias = true;
+    enableMan = true;
+
+    # Clip-board syncing
+    waylandSupport = true;
+    clipboard.register = "unnamedplus";
+
+    # Language providers
+    withNodeJs = true;
+    withPerl = true;
+    withPython3 = true;
+    withRuby = true;
+
+    editorconfig = {
+      enable = true;
+    };
+
+    colorschemes = {
+      onedark = {
+        enable = true;
+      };
+    };
+
+    opts = {
+      #  -----GENERAL-----
+      encoding = "UTF-8";
+      backspace = "indent,eol,start";
+
+      # Set language for spell check
+      spell = true;
+
+      # Fixing tabs and indentation
+      autoindent = true;
+      copyindent = true;
+      expandtab = false;
+      preserveindent = true;
+      softtabstop = 0;
+
+      # Case-insensitive tab completion
+      wildignorecase = true;
+
+      # Make scrolling faster and more accurate
+      ttyfast = true;
+
+      # Refresh buffer automatically
+      autoread = true;
+
+      # Add mouse support
+      mouse = "a";
+
+      # Suppress warning when changing FROM unsaved buffer
+      hidden = true;
+
+      # Demand explicit confirmation when closing unsaved buffers
+      confirm = true;
+
+      # Deactivate folding
+      foldenable = false;
+      foldmethod = "syntax";
+
+      # Don't use swapfiles
+      swapfile = false;
+    };
+
+    localOpts = {
+      # Set language for spell check
+      spelllang = "en_us";
+    };
+  };
+}
