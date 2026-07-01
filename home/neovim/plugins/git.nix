48577da70ff8b35e9568dd417c7904cae2cc2dc2
Rudolf Tucek <tucek.rudolf@gmail.com>
2026-07-01 22:08:58 +0200

    Avoid signcolumn jumps

--- home/neovim/plugins/git.nix
+++ home/neovim/plugins/git.nix
@@ -1,6 +1,12 @@
 { ... }:
 {
   programs.nixvim = {
+    opts = {
+      # Set signcolumn to be always present.
+      # This way, we don't have awkward flash jumps.
+      signcolumn = "yes";
+    };
+
     plugins = {
       # see https://github.com/airblade/vim-gitgutter/
       gitgutter = {
