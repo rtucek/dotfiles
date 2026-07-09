9f6f8b5784deacb218c847c57e29a692711c7ca9
Rudolf Tucek <tucek.rudolf@gmail.com>
2026-07-09 03:04:36 +0200

    Fix golang dependency of gopls LSP

--- home/neovim/lsp/golang.nix
+++ home/neovim/lsp/golang.nix
@@ -1,6 +1,9 @@
 {
   programs.nixvim = {
+    dependencies.go.enable = true;
+
     lsp.servers = {
+      # see https://github.com/golang/tools/tree/master/gopls
       gopls = {
         enable = true;
       };
