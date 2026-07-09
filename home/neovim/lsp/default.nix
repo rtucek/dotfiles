422478c64327380d9b0adb684e163864c0dfbecb
Rudolf Tucek <tucek.rudolf@gmail.com>
2026-07-09 03:05:14 +0200

    Tweak LSP integration
    The following changes, related to LSP make neovim feel more like an IDE:
    - Add ultisnips plugin
    - Format code on-save
    - Show LSP diagnostic errors
    - Show current function signature + highlight active parameter
    - Add proper LSP key-mappings
    - Add CMP plugin + ultisnips integration for auto-complete

--- home/neovim/lsp/default.nix
+++ home/neovim/lsp/default.nix
@@ -1,68 +1,110 @@
-{ config, ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./golang.nix
-    ./nix.nix
-    ./sh.nix
+    # ./nix.nix
+    # ./sh.nix
   ];
 
   programs.nixvim = {
-    lsp =
-      let
-        nixvimLua = config.lib.nixvim.mkRaw;
-      in
-      {
-        inlayHints.enable = true;
-        keymaps = [
-          {
-            key = "gd";
-            lspBufAction = "definition";
-          }
-          {
-            key = "gD";
-            lspBufAction = "references";
-          }
-          {
-            key = "gt";
-            lspBufAction = "type_definition";
-          }
-          {
-            key = "gi";
-            lspBufAction = "implementation";
-          }
-          {
-            key = "K";
-            lspBufAction = "hover";
-          }
-          {
-            action = nixvimLua "function() vim.diagnostic.jump({ count=-1, float=true }) end";
-            key = "<leader>k";
-          }
-          {
-            action = nixvimLua "function() vim.diagnostic.jump({ count=1, float=true }) end";
-            key = "<leader>j";
-          }
-          {
-            action = "<CMD>LspStop<Enter>";
-            key = "<leader>lx";
-          }
-          {
-            action = "<CMD>LspStart<Enter>";
-            key = "<leader>ls";
-          }
-          {
-            action = "<CMD>LspRestart<Enter>";
-            key = "<leader>lr";
-          }
-          {
-            action = nixvimLua "require('telescope.builtin').lsp_definitions";
-            key = "gd";
-          }
-          {
-            action = "<CMD>Lspsaga hover_doc<Enter>";
-            key = "K";
-          }
-        ];
+    extraPlugins = with pkgs.vimPlugins; [
+      ultisnips
+    ];
+
+    autoGroups = {
+      format_on_save = {
+        clear = true;
       };
+    };
+    autoCmd = [
+      {
+        event = [ "BufWritePre" ];
+        buffer = 0;
+        command = "lua vim.lsp.buf.format()";
+        desc = "Auto-format on save";
+        group = "format_on_save";
+      }
+    ];
+
+    # Required for the `lsp-lines` plugin
+    diagnostic = {
+      settings = {
+        virtual_lines = true;
+        virtual_text = false;
+      };
+    };
+
+    highlight = {
+      LspSignatureActiveParameter = {
+        fg = "#282c34";
+        bg = "#61afef";
+      };
+    };
+
+    plugins = {
+      # see https://github.com/neovim/nvim-lspconfig/
+      lsp = {
+        enable = true;
+        keymaps = {
+          lspBuf = {
+            K = "hover";
+            gD = "references";
+            gd = "definition";
+            gi = "implementation";
+            gt = "type_definition";
+          };
+        };
+      };
+
+      # see https://git.sr.ht/~whynothugo/lsp_lines.nvim
+      lsp-lines = {
+        enable = true;
+      };
+
+      # see https://github.com/ray-x/lsp_signature.nvim/
+      lsp-signature = {
+        enable = true;
+        settings = {
+          hint_enable = false;
+        };
+      };
+
+      # see https://github.com/hrsh7th/nvim-cmp
+      cmp = {
+        enable = true;
+        autoEnableSources = true;
+        settings = {
+          sources = [
+            { name = "nvim_lsp"; }
+            { name = "buffer"; }
+            { name = "path"; }
+            { name = "ultisnips"; }
+          ];
+
+          snippet = {
+            expand = ''
+              function(args)
+                -- ultisnips
+                vim.fn["UltiSnips#Anon"](args.body)
+              end
+            '';
+          };
+
+          mapping = {
+            "<C-Space>" = "cmp.mapping.complete()";
+            "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+            "<C-e>" = "cmp.mapping.close()";
+            "<C-f>" = "cmp.mapping.scroll_docs(4)";
+            "<CR>" = "cmp.mapping.confirm({ select = true })";
+            "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+            "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+          };
+        };
+      };
+      # see https://github.com/quangnguyen30192/cmp-nvim-ultisnips/
+      cmp-nvim-ultisnips = {
+        enable = true;
+      };
+    };
   };
 }
