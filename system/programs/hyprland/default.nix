ce2950d496bfd6f545ef2dd6af137c0a2b158038
Rudolf Tucek <tucek.rudolf@gmail.com>
2026-07-11 01:41:00 +0200

    Change cursor theme to adwaita

--- system/programs/hyprland/default.nix
+++ system/programs/hyprland/default.nix
@@ -10,7 +10,14 @@ in
 {
   environment.systemPackages = [
     pkgs.hyprland
+    # For adwaita cursor theme
+    pkgs.adwaita-icon-theme
   ];
+
+  environment.sessionVariables = {
+    XCURSOR_THEME = "Adwaita";
+    XCURSOR_SIZE = "24";
+  };
 
   programs.hyprland = {
     enable = true;
