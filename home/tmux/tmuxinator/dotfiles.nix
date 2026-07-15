c175b381cfef268b10f50af92bdb1b44be0d59d5
Rudolf Tucek <tucek.rudolf@gmail.com>
2026-07-15 16:55:59 +0200

    tmuxinator: clone dotfile repo if not already present

--- home/tmux/tmuxinator/dotfiles.nix
+++ home/tmux/tmuxinator/dotfiles.nix
@@ -1,9 +1,29 @@
-{ pkgs, config, ... }:
 {
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+let
+  targetDir = "${config.home.homeDirectory}/projects/dotfiles";
+  repoUrl = "https://github.com/rtucek/dotfiles.git";
+  git = "${lib.getExe pkgs.git}";
+  mkdir = "${lib.getBin pkgs.coreutils-full}/bin/mkdir";
+in
+{
+  home.activation.cloneDotfiles = lib.hm.dag.entryAfter [ "writeBoundary" "installPackages" "git" ] ''
+    # Only clone if the .git directory doesn't exist
+    # This preserves local changes if the repo already exists
+    if [ ! -d "${targetDir}/.git" ]; then
+      ${mkdir} -p ${targetDir}
+      ${git} clone ${repoUrl} ${targetDir}
+    fi
+  '';
+
   programs.tmux.tmuxinator.projects = {
     dotfiles = {
       name = "dotfiles";
-      root = "~/projects/dotfiles/";
+      root = "${targetDir}/";
 
       startup_window = "git";
 
