4f9d2675394ca519daac2938f7927494e1589bbe
Rudolf Tucek <tucek.rudolf@gmail.com>
2026-08-01 20:34:29 +0200

    Remove tzupdate systemd service
    tzupdate can prevent nixos system rebuilds as it typically fires upon
    activation. In case internet is not accessible, the service fails, causing nixos
    refusing to apply the switch.
    
    On the other hand, `systemd-timesyncd.service` already takes care of keeping the
    local time in sync via NTP.

--- system/core/time.nix
+++ system/core/time.nix
@@ -1,14 +1,8 @@
 { lib, ... }:
 {
-  # Set a sensitive default, which shall be overridden via
-  # `services.automatic-timezoned.enable`, based on the current geolocation.
-  #
-  # see https://search.nixos.org/options?channel=unstable&query=services.automatic-timezoned.enable
-  # for details.
   time.timeZone = lib.mkDefault "Europe/Vienna";
 
   services = {
     automatic-timezoned.enable = true;
-    tzupdate.enable = true;
   };
 }
