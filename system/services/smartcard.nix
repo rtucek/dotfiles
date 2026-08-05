64db144411aaabdf74cdbd5feefe8202fc85d3ac
Rudolf Tucek <tucek.rudolf@gmail.com>
2026-08-06 01:13:03 +0200

    Revert "GPG: remove pcscd in favor of scdaemon"
    This reverts commit 5d5dba92ad77f3d8328442c0212a46bb79f72235.

--- system/services/smartcard.nix
+++ system/services/smartcard.nix
@@ -1,10 +1,16 @@
-{ lib, ... }:
+{ ... }:
 {
   programs.yubikey-manager.enable = true;
 
-  # We will use GPG's scdaemon instead.
-  services.pcscd.enable = lib.mkForce false;
-
+  # pcscd will be used as the authoritative daemon, interacting with smartcards
+  # like Yubikeys.
+  #
+  # However, pcscd does try to acquirer an exclusive lock on a detected
+  # smartcard and so do other daemons (e.g. scdaemon from GPG).
+  # As such, we have to make sure that other daemons work with pcscd together.
+  #
+  # see https://github.com/OpenSC/OpenSC/wiki/GnuPG-and-OpenSC
+  services.pcscd.enable = true;
   # Add udev rules for smart cards
   hardware.gpgSmartcards.enable = true;
 
