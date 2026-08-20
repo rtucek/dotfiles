0b16c9a4e3e9e67b2c8764d79cd266292951b065
Rudolf Tucek <tucek.rudolf@gmail.com>
2026-08-20 10:56:44 +0200

    dell-prec-5570-nixos-btw: define LVM partition sizes

--- hosts/dell-prec-5570-nixos-btw/default.nix
+++ hosts/dell-prec-5570-nixos-btw/default.nix
@@ -8,4 +8,11 @@
 
   networking.hostName = "dell-prec-5570-nixos-btw";
   sops.defaultSopsFile = ../../secrets/hosts/dell-prec-5570-nixos-btw.yaml;
+
+  disko.devices.lvm_vg.volgroup0.lvs = {
+    # 250 GB of available disk space
+    lv_root.size = "250G";
+    # 250 GB of available disk space
+    lv_home.size = "250G";
+  };
 }
