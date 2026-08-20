33c60e7a8308f1775a34e6cf0737277ddd09a8af
Rudolf Tucek <tucek.rudolf@gmail.com>
2026-08-20 10:58:11 +0200

    dell-prec-5570-nixos-btw: hyprland: configure eDP-1 screen

--- hosts/dell-prec-5570-nixos-btw/default.nix
+++ hosts/dell-prec-5570-nixos-btw/default.nix
@@ -9,6 +9,19 @@
   networking.hostName = "dell-prec-5570-nixos-btw";
   sops.defaultSopsFile = ../../secrets/hosts/dell-prec-5570-nixos-btw.yaml;
 
+  home-manager.users.rtucek = {
+    wayland.windowManager.hyprland.settings = {
+      monitor = [
+        {
+          output = "eDP-1";
+          mode = "1900x1200@59.9500";
+          position = "0x0";
+          scale = 1;
+        }
+      ];
+    };
+  };
+
   disko.devices.lvm_vg.volgroup0.lvs = {
     # 250 GB of available disk space
     lv_root.size = "250G";
