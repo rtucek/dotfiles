098ea50b1425bd7affb3001f5f7e40eb262bd34d
Rudolf Tucek <tucek.rudolf@gmail.com>
2026-08-22 14:36:24 +0200

    tux-ibp-amdgen9-nixos-btw: adopt rtucek user model

--- hosts/tux-ibp-amdgen9-nixos-btw/default.nix
+++ hosts/tux-ibp-amdgen9-nixos-btw/default.nix
@@ -2,30 +2,28 @@
 {
   imports = [
     ./hardware-configuration.nix
-    ../common/users/rtucek.nix
     inputs.nixos-hardware.nixosModules.tuxedo-infinitybook-pro14-gen9-amd
+    ../../modules
   ];
 
   networking.hostName = "tux-ibp-amdgen9-nixos-btw";
   sops.defaultSopsFile = ../../secrets/private/hosts/tux-ibp-amdgen9-nixos-btw.yaml;
-
-  home-manager.users.rtucek = {
-    wayland.windowManager.hyprland.settings = {
-      monitor = [
-        {
-          output = "eDP-1";
-          mode = "2880x1800@120.0000";
-          position = "0x0";
-          scale = 1.5;
-        }
-      ];
-    };
-  };
 
   disko.devices.lvm_vg.volgroup0.lvs = {
     # 250 GB of available disk space
     lv_root.size = "250G";
     # 250 GB of available disk space
     lv_home.size = "250G";
+  };
+
+  rtucek = {
+    hyprland.monitors = [
+      {
+        output = "eDP-1";
+        mode = "2880x1800@120.0000";
+        position = "0x0";
+        scale = 1.5;
+      }
+    ];
   };
 }
